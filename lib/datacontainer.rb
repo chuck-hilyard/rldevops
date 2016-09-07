a5898ba30a18f4e9a46616f7e0fb16888c612872
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-09-06 21:00:17 -0700

    adding data container

--- lib/datacontainer.rb
+++ lib/datacontainer.rb
@@ -1,8 +1,14 @@
 
 class DataContainer
 
-    def initialize 
+    def initialize(*args) 
         print "DataContainer object initialization\n"
+        if args.length < 1
+            abort("not running w/o arguments")
+        else
+            print "processing arguments\n"
+        end
+
     end
 
 end
