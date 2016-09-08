315f412043e093942def42960a078609beceaf1b
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-09-07 17:49:08 -0700

    updating datacontainer

--- lib/datacontainer.rb
+++ lib/datacontainer.rb
@@ -4,7 +4,7 @@ class DataContainer
     def initialize(*args) 
         print "DataContainer object initialization\n"
         if args.length < 1
-            abort("not running w/o arguments")
+            abort("can't run w/o arguments")
         else
             print "processing arguments\n"
         end
