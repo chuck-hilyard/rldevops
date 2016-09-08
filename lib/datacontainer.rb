182516ddbd464f69f265ef46f8e12d12965d44ad
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-09-08 16:34:37 -0700

    building out the datacontainer

--- lib/datacontainer.rb
+++ lib/datacontainer.rb
@@ -1,3 +1,9 @@
+# this will load our environment configuration into an object.  
+# 
+# use puppet to manage the .json file
+# 
+
+require 'JSON'
 
 class DataContainer
 
@@ -6,7 +12,13 @@ class DataContainer
         if args.length < 1
             abort("can't run w/o arguments")
         else
+            print "loading environment values\n"
+            configfile = File.read('datacontainer.json')
+            confighash = JSON.parse(configfile)
+            # load environments hash from json here
+            
             print "processing arguments\n"
+            # check the args passed against the hash loaded from the json config file
         end
 
     end
