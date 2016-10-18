31929ce759a644f6e136635f1c448f9d3e4a2852
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-10-17 17:22:37 -0700

    updates

--- lib/authorizationhandler.rb
+++ lib/authorizationhandler.rb
@@ -5,11 +5,14 @@
 
 class AuthorizationHandler
 
+    attr_reader :authorized
+
     def initialize
         print "AuthorizationHandler object initialization\n"
         ## check DataContainer::environment if 'prod'
         # then check DataContainer::jira_ticket_number
         ## need to refactor ticketing/jira/resthandler.rb to support object creation
+        @authorized = 0
     end
 
 end
--- runtime.rb
+++ runtime.rb
@@ -2,13 +2,12 @@
 require_relative 'lib/authenticationhandler.rb'
 require_relative 'lib/datacontainer.rb'
 require_relative 'lib/authorizationhandler.rb'
-require_relative 'lib/runcontrol.rb'
 
 
 class RunTimeHandler
 
     def initialize
-        print "DevOpsLoader.initialize()\n"
+        print "RunTimeHandler.initialize()\n"
         auth_handler = AuthenticationHandler.new 
         data_container = DataContainer.new
         authorization_handler = AuthorizationHandler.new
