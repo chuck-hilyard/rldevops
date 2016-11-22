fec3503d6a021a08060d3bbd082979b2a56818a9
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-11-22 03:24:58 -0800

    updates

--- contents/tests/test_datacontainer.rb
+++ contents/tests/test_datacontainer.rb
@@ -5,10 +5,5 @@ require_relative '../lib/datacontainer.rb'
 
 dc = DataContainer.new('/Users/chuck.hilyard/projects/rldevops-plugin/contents')
 
-# this works do not delete
-#puts dc.configjson['platform_datacenter']["usa"]
-
-# even better.  let's pass the key we'd like to check and see if it returns true (the key is valid) or false (the key is invalid)
-# OR a return value (key, value)
 key, value = dc.key_check("lax")
 printf("return key & value: %s %s\n", key, value)
