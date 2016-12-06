6890aaec7f4157ac62b0b342bc106a3a964ab74b
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2016-12-05 17:19:13 -0800

    google docs api

--- contents/lib/googlesheetsapi.rb
+++ contents/lib/googlesheetsapi.rb
@@ -1,9 +1,16 @@
 
-
+# test sheet #
+# https://docs.google.com/spreadsheets/d/1JRfAZfmdtOHOWc3NofTkXVMofKoKs1xCWBFWo6P3wg0/edit#gid=0
+# spreadsheetId=1JRfAZfmdtOHOWc3NofTkXVMofKoKs1xCWBFWo6P3wg0
+# sheetId=0
+#
+# passable curl string
+# https://sheets.googleapis.com/v4/spreadsheets/1JRfAZfmdtOHOWc3NofTkXVMofKoKs1xCWBFWo6P3wg0/values/A2%3AB2?dateTimeRenderOption=SERIAL_NUMBER&majorDimension=DIMENSION_UNSPECIFIED&valueRenderOption=FORMATTED_VALUE&key=AIzaSyD59fSXFLZty357UAMlSrmBBj6qzKxFyEQ
 
 class GoogleSheetsAPI
     
     def initialize
         print "GoogleSheetsAPI.initialize\n"
     end
+
 end
